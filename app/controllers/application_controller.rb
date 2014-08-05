ac904a016fcf83544c940e2fe2a83ebd15fc98b1
Pablo Faust <pabfaust@gmail.com>
2014-08-05 14:07:22 -0500

    404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,18 @@
 class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
+
+   def show
+    @product = Product.find(params[:id])
+ 
+    # If the request is stale according to the given timestamp and etag value
+    # (i.e. it needs to be processed again) then execute this block
+    if stale?(last_modified: @product.updated_at.utc, etag: @product.cache_key)
+      respond_to do |wants|
+        # ... normal response processing
+      end
+    end
+
   protect_from_forgery with: :exception
+end
 end
